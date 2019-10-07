2612e8ff5ef7051d695ad48e47b9222cba78ada9
Heavy-Testnet-Wolf <55656453+Heavy-Testnet-Wolf@users.noreply.github.com>
2019-10-07 21:58:53 +0200

    Update UsefulFunctions.py

--- UsefulFunctions.py
+++ UsefulFunctions.py
@@ -17,3 +17,33 @@ def btc2str(number,unit='XBT'):
 def pct2str(number,decimals=2):
     # Convert a percentage to a string
     return str(roundExact(number,decimals))
+
+def seconds2str(seconds):
+    seconds = round(seconds)
+    h = seconds//(60*60)
+    m = (seconds-h*60*60)//60
+    s = seconds-(h*60*60)-(m*60)
+    if h > 0:
+        return num2strPostFix(h,'hour')+' and '+num2strPostFix(m,'minute')
+    if m > 0:
+        return num2strPostFix(m,'minute')+' and '+num2strPostFix(s,'second')
+    return num2strPostFix(s,'second')
+
+def num2strPostFix(number,string):
+    if abs(number) != 1:
+        string += 's'
+    return str(number)+' '+string
+
+def extractSubstring(raw_string, start_marker, end_marker):
+    start = raw_string.find(start_marker) + len(start_marker)
+    end = raw_string.find(end_marker, start)
+    if start == -1 or end == -1:
+        return ''
+    return raw_string[start:end]
+
+def extractSubstringFromRight(raw_string, start_marker, end_marker):
+    start = raw_string.rfind(start_marker) + len(start_marker)
+    end = raw_string.rfind(end_marker, start)
+    if start == -1 or end == -1:
+        return ''
+    return raw_string[start:end]
